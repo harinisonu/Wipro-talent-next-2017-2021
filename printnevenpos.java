deee77b91272ca6978f8534154c365b24c1d99f6
Harini B R <harinisonubr@gmail.com>
2020-05-26 23:12:41 +0530

    Update printnevenpos.java

--- printnevenpos.java
+++ printnevenpos.java
@@ -4,13 +4,11 @@ public class printnevenpos
 {
 	public static void main(String[] args) {
 		Scanner sc =  new Scanner(System.in);
-		//int a = sc.nextInt();//getting input from user
-		//int b = sc.nextInt();//getting input from user
-		int n = 0;//intiallizing n is 1
+		int n = 0;//intiallizing n is 0
 		while(n<=100)//looping 100 times
 		{
-		    System.out.println(n);//print numbers from 1 tp 100
-		    n+=2;//incrementing value of n
+		    System.out.println(n);//print numbers from 0 to 100
+		    n+=2;//incrementing value of n by 2
 		}
 	}
 }
