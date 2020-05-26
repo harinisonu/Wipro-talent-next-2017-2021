baf03eebe11374bd3d483b3fa505623655958a08
Harini B R <harinisonubr@gmail.com>
2020-05-26 22:53:31 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,15 @@
+/* program to accept a number N and print whether the number is EVEN or ODD*/
+import java.util.*;
+public class evenodd
+{
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		int n = sc.nextInt();//getting input from user
+		if(n%2 == 0)//if n is even
+		{
+		    System.out.println("EVEN");//print even
+		}
+		else
+		    System.out.println("ODD");//if n is odd print odd
+	}
+}
