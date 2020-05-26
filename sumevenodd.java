c04351abadc466c5af0e50035969e044bc2fb964
Harini B R <harinisonubr@gmail.com>
2020-05-26 22:59:18 +0530

    Add files via upload

---
+++ sumevenodd.java
@@ -0,0 +1,17 @@
+/* program to accept two numbers and print whether their sum is EVEN or ODD */
+import java.util.*;
+public class sumevenodd
+{
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		int a = sc.nextInt();//getting input from user
+		int b = sc.nextInt();//getting input from user
+		int n = a+b;
+		if(n%2 == 0)//if n is even
+		{
+		    System.out.println("EVEN");//print even
+		}
+		else
+		    System.out.println("ODD");//if n is odd print odd
+	}
+}
