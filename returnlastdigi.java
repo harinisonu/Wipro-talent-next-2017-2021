6e0d583cc75d40d53d6989a14fe218239f788d4c
Harini B R <harinisonubr@gmail.com>
2020-05-28 15:59:08 +0530

    Add files via upload

---
+++ returnlastdigi.java
@@ -0,0 +1,18 @@
+package kexa;
+import java.util.*;
+public class returnlastdigi
+{
+	public static void main(String args[]) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int n;
+		n = sc.nextInt();
+		System.out.println(lastdigi(n));	//odd function called and printed	
+	}
+	private static int lastdigi(int n) // odd function declaration
+	{
+		int a = n%10;  // modulus gives reminder which is the last digit
+		return a;	// returns last digit of a no
+	}
+
+}
