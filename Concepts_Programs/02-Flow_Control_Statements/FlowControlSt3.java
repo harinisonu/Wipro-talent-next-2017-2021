e633e04ead4b1c951d7651aea0d8e0767b1b1d4b
Harini B R <harinisonubr@gmail.com>
2020-06-02 00:12:02 +0530

    Update FlowControlSt3.java

--- Concepts_Programs/02-Flow_Control_Statements/FlowControlSt3.java
+++ Concepts_Programs/02-Flow_Control_Statements/FlowControlSt3.java
@@ -1,3 +1,14 @@
+/******************************************
+
+Write a program to check if the program has received command line arguments or not. If the
+program has not received the values then print "No Values", else print all the values in a single line
+separated by ,(comma).
+Eg1) java Example
+O/P: No values
+Eg2) java Example Mumbai Bangalore
+O/P: Mumbai,Bangalore
+
+******************************************/
 public class FlowControlSt3
 {
 	public static void main(String[] args) 
