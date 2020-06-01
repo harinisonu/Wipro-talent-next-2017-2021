7ae5098df35b08e2954bc40e6438027dd7c51d4f
Harini B R <harinisonubr@gmail.com>
2020-06-01 23:09:45 +0530

    Create FlowControlSt3.java

---
+++ Concepts_Programs/02-Flow_Control_Statements/FlowControlSt3.java
@@ -0,0 +1,18 @@
+public class FlowControlSt3
+{
+	public static void main(String[] args) 
+	{		
+		if(args.length == 0)
+		{
+			System.out.print("No Values");
+		}
+		else
+		{
+			for(int i=0; i<args.length;i++)
+			{
+				System.out.print(args[i]+",");
+			}
+		}
+
+	}
+}
