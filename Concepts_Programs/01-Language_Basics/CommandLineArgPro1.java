fb3e3bc8beecd59968799865481b8514bfda721d
Harini B R <harinisonubr@gmail.com>
2020-06-01 22:17:49 +0530

    Create CommandLineArgPro1.java

---
+++ Concepts_Programs/01-Language_Basics/CommandLineArgPro1.java
@@ -0,0 +1,13 @@
+/* Write a Program that accepts two Strings as command line arguments and generate the output in
+a specific way as given below.*/
+
+import java.util.*;
+public class CommandLineArgPro1
+{
+	public static void main(String[] args) 
+	{
+	    System.out.println(args[0]+" Technology "+args[1]);  
+		
+	}
+
+}
