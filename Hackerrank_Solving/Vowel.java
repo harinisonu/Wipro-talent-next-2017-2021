9e74f44be1bc824dca749ed79b50d46de4645adc
Harini B R <harinisonubr@gmail.com>
2020-06-01 23:04:39 +0530

    Add files via upload

---
+++ Hackerrank_Solving/Vowel.java
@@ -0,0 +1,24 @@
+/* Hackerrank problem 1 dated 26-05-2020*/
+import java.util.*;
+class Vowel {
+    public static void main(String[] args) {
+        int flag = 0;
+        Scanner sc =  new Scanner(System.in);
+        String a = sc.nextLine();//getting string as input
+        char[] ch = a.toCharArray();//converting strings into char
+        TreeSet hs = new TreeSet();//creating hashset
+        /* using enhanced forloop to acces the char*/
+        for (char c : ch){
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            {
+               hs.add(c);
+               flag = 1;
+               //adding all vowels from string in hashset
+            }
+        }
+        //using foreach loop tpo print the hashset
+        hs.forEach(System.out::print);  
+        if(flag == 0)//no vowels in string 
+            System.out.println("Thank You");
+    }
+}
