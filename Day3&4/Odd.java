8143db58967a08dfc10a14ee8c43916f9dcc1fc3
Harini B R <harinisonubr@gmail.com>
2020-06-01 22:54:53 +0530

    Add files via upload

---
+++ Day3&4/Odd.java
@@ -0,0 +1,23 @@
+/* write a func to find whether given input no is odd */
+import java.util.*;
+public class Odd 
+{
+	public static void main(String args[]) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int n;
+		n = sc.nextInt();
+		System.out.println(Odd(n));	//odd function called and printed	
+	}
+	private static int Odd(int n) // odd function declaration
+	{
+		if(n%2==0)
+		{
+			return 1;  // if n is even returns 1
+		}
+		else
+			return 2; // if n is odd return 2
+		
+	}
+
+}
