539e473d4871e1c4bfc86a2a19d391093cb03b7b
Harini B R <harinisonubr@gmail.com>
2020-06-02 00:29:47 +0530

    Update CommandLineArgPro3.java

--- Concepts_Programs/01-Language_Basics/CommandLineArgPro3.java
+++ Concepts_Programs/01-Language_Basics/CommandLineArgPro3.java
@@ -7,7 +7,8 @@ public class CommandLineArgPro3
 	public static void main(String[] args) 
 	{
 		int sum=0;
-		sum = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+		sum = Integer.parseInt(args[0]) + Integer.parseInt(args[1]); 
+		/*converting String to integer as command line argument always returns string.*/
 	    System.out.println(" The Sum Of  "+Integer.parseInt(args[0])+" and "+Integer.parseInt(args[1])+" is "+ sum);  
 		
 	}
