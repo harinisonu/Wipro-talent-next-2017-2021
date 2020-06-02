d1b4e1575a2eb8a2bd1d90b074bdaf988698c007
Harini B R <harinisonubr@gmail.com>
2020-06-02 15:44:34 +0530

    Create FlowControlSt11.java

---
+++ Concepts_Programs/02-Flow_Control_Statements/FlowControlSt11.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ 
+11)
+
+Write a program to print even numbers between 23 and 57, each number should be printed in a
+separate row.
+Topics Covered: For Loop
+ 
+ *******************************************************************************/
+public class FlowControlSt11
+{
+	public static void main(String[] args) 
+	{		
+		for(int i = 24; i < 57; )//looping 10 times
+		{
+			System.out.println(i);//print numbers from 1 to 10
+			i = i+2;
+		}		
+		
+	}
+}
