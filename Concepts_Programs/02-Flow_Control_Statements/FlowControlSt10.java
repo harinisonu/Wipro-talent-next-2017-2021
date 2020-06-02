6042e67f66003e555d5c1ea54979bcb41c53827c
Harini B R <harinisonubr@gmail.com>
2020-06-02 15:32:57 +0530

    Create FlowControlSt10.java

---
+++ Concepts_Programs/02-Flow_Control_Statements/FlowControlSt10.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ 
+10)
+
+Write a program to print numbers from 1 to 10 in a single row with one tab space.
+Topics Covered: For Loop
+ 
+ *******************************************************************************/
+public class FlowControlSt10
+{
+	public static void main(String[] args) 
+	{		
+		for(int i=1; i <= 10; i++)//looping 10 times
+		{
+			System.out.print(i+" ");//print numbers from 1 to 10
+		}		
+		
+	}
+}
